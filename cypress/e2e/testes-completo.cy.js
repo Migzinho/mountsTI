d6bb302bd552192999fb03741e4570955437a06a
Migzinho <emidio.mignozzetti@gmail.com>
2025-06-13 02:14:45 -0300

    chore: initial commit

---
+++ cypress/e2e/testes-completo.cy.js
@@ -0,0 +1,75 @@
+describe('💻 3 Testes Interface do Serverest', () => {
+ 
+  it('🔐 Cadastro de usuário pela interface sendo admin - válido', () => {
+  cy.fixture('usuario').then((usuario) => {
+    cy.cadastrarUsuarioUI(usuario.nome, usuario.email, usuario.password, usuario.administrador)
+  })
+})
+
+it('❌ Cadastro de usuário duplicado', () => {
+  cy.fixture('usuario').then((usuario) => {
+    cy.cadastrarUsuarioDuplicadoUI(usuario.nome, usuario.email, usuario.password, usuario.administrador)
+  })
+})
+
+  it('📦 Cadastro de produto pela interface', () => {
+  cy.fixture("login").then((login) => {
+    cy.loginUI(login.email, login.senha)
+  cy.fixture("produto").then((produto) => {
+    cy.cadastrarProdutoUI(produto)
+    })
+  })
+})
+})
+
+describe('🔌3 Testes de API - Serverest', () => {
+  let token = ''
+  let produtoId = ''
+
+  it('✅ Login via API deve retornar token', () => {
+    cy.request('POST', 'https://serverest.dev/login', {
+      email: 'emidio@mignozzetti.com',
+      password: '123456'
+    }).then((res) => {
+      expect(res.status).to.eq(200)
+      expect(res.body).to.have.property('authorization')
+      token = res.body.authorization
+    })
+  })
+
+  it('🛒 Cadastro de produto via API com token', () => {
+    cy.fixture('produto').then((produto) => {
+      cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/produtos',
+        headers: { Authorization: token },
+        body: produto,
+        failOnStatusCode: false
+      }).then((res) => {
+        expect(res.status).to.be.oneOf([201, 400]) // 400 se já existir
+        if (res.status === 201) {
+          expect(res.body.message).to.eq('Cadastro realizado com sucesso')
+        }
+        if (res.status === 400) {
+          expect(res.body.message).to.include('já existe')
+        }
+      })
+    })
+  })
+
+    it.only('✅ Deve retornar a lista de usuários cadastrados', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://serverest.dev/usuarios'
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('quantidade')
+      expect(response.body).to.have.property('usuarios')
+      expect(response.body.usuarios).to.be.an('array')
+
+      cy.log(`Foram retornados ${response.body.quantidade} usuários.`)
+    })
+  })
+})
+
+
---
+++ cypress/support/commands.js
@@ -0,0 +1,62 @@
+Cypress.Commands.add('cadastrarUsuarioUI', (nome, email, senha) => {
+const mensagemEsperada = 'Bem Vindo  Emidio'
+
+  cy.visit('/cadastrarusuarios')
+  cy.get('input[name="nome"]').type(nome)
+  cy.get('input[name="email"]').type(email)
+  cy.get('input[name="password"]').type(senha)
+  cy.get('input[type="checkbox"]').check()
+    .should('be.checked') // Verificação se o checkbox Administrador esta marcado
+  cy.get('button[type="submit"]').click()
+  // Verificação da mensagem retornada  
+  cy.contains('×Cadastro realizado com sucesso').should('be.visible')
+  cy.url().should('eq', 'https://front.serverest.dev/admin/home') 
+  cy.get('h1').should('have.text', mensagemEsperada)
+})
+
+Cypress.Commands.add('cadastrarUsuarioDuplicadoUI', (nome, email, senha) => {
+const mensagemEsperada = 'Este email já está sendo usado'
+
+  cy.visit('/cadastrarusuarios')
+  cy.get('input[name="nome"]').type(nome)
+  cy.get('input[name="email"]').type(email)
+  cy.get('input[name="password"]').type(senha)
+  cy.get('input[type="checkbox"]').check()
+    .should('be.checked')
+  cy.get('button[type="submit"]').click()
+  // Verificação da mensagem retornada  
+  cy.get('.alert > :nth-child(2)').should('have.text', mensagemEsperada)
+})
+
+Cypress.Commands.add('loginUI', (email, senha) => {
+  cy.visit('/')
+  cy.get('input[name="email"]').type(email)
+  cy.get('[data-testid="senha"]').type(senha)
+  cy.get('[data-testid="entrar"]').click()
+  // Verificações
+  cy.url().should('eq', 'https://front.serverest.dev/admin/home')
+})
+
+Cypress.Commands.add('cadastrarProdutoUI', (produto) => {
+  cy.contains('Produtos').click()
+  cy.get('input[name="nome"]').type(produto.nome)
+  cy.get('[data-testid="preco"]').type(produto.preco.toString())
+  cy.get('[data-testid="descricao"]').type(produto.descricao)
+  cy.get('[data-testid="quantity"]').type(produto.quantidade.toString())
+  cy.get('[data-testid="cadastarProdutos"]').click()
+  // Verificações
+  cy.url().should('eq', 'https://front.serverest.dev/admin/listarprodutos')
+  cy.get('h1').should('have.text', 'Lista dos Produtos')
+})
+
+Cypress.Commands.add('criarProdutoApi', (produto) => {
+  const token = window.localStorage.getItem('token')
+  cy.request({
+    method: 'POST',
+    url: 'https://serverest.dev/produtos',
+    headers: { Authorization: token },
+    body: produto
+  }).then((res) => {
+    expect(res.status).to.eq(201)
+  })
+})
